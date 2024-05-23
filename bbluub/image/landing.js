8f67f1c44aac008bb290a4e164d126aab3bdf0dc
Duck-life <benazzouz788@gmail.com>
2024-05-23 14:51:45 +0200

    LAST VERSION

--- bbluub/image/landing.js
+++ bbluub/image/landing.js
@@ -1,24 +1,48 @@
-let slideIndex = 1;
-showSlide(slideIndex);
+let slideIndex = 1
+showSlides(slideIndex)
 
-function moveSlide(n) {
-    showSlide(slideIndex += n);
+// Next/previous controls
+function plusSlides (n) {
+  showSlides((slideIndex += n))
 }
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("carousel-slide");
-    
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
+// Thumbnail image controls
+function currentSlide (n) {
+  showSlides((slideIndex = n))
+}
+
+function showSlides (n) {
+  let i
+  let slides = document.getElementsByClassName('slides')
+  let dots = document.getElementsByClassName('dot')
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += ' active'
+}
+
+automaticShowSlides()
+
+function automaticShowSlides () {
+  let i
+  let slides = document.getElementsByClassName('slides')
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  setTimeout(automaticShowSlides, 3000) // Change image every 3 seconds
 }
